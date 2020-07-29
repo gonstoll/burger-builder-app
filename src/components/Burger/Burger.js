646f3398c930cd710276b0314e84caa62c75d8c8
Gonzalo Stoll <stollgonzalo@gmail.com>
2020-07-29 18:21:55 +0200

    Dynamic burger ingredients - propTypes added

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = props => {
+    const ingredientsArray = Object.keys(props.ingredients);
+    let ingredients = ingredientsArray
+		.map(ingredient => {
+			return [...Array(props.ingredients[ingredient])].map((_, i) => {
+				return <BurgerIngredient key={ingredient + i} type={ingredient} />
+			});
+		})
+		.reduce((arr, el) => arr.concat(el), []);
+
+    if (ingredients.length === 0) {
+        ingredients = <p>Please start adding ingredients!</p>;
+    }
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {ingredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+};
+
+burger.propTypes = {
+    ingredients: PropTypes.object
+};
+
+export default burger;
